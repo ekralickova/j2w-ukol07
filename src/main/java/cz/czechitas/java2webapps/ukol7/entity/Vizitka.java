a78d849896d83e6b997df4f0b307a4e27337b971
ekralickova <kralickova.eva@gmail.com>
2021-05-21 20:58:11 +0200

    kralickova ukol 7

---
+++ src/main/java/cz/czechitas/java2webapps/ukol7/entity/Vizitka.java
@@ -0,0 +1,140 @@
+package cz.czechitas.java2webapps.ukol7.entity;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Entity
+public class Vizitka {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Length(max = 100)
+    @NotBlank
+    private String celeJmeno;
+
+    @Length(max = 100)
+    @NotBlank
+    private String firma;
+
+    @Length(max = 100)
+    @NotBlank
+    private String ulice;
+
+    @Length(max = 100)
+    @NotBlank
+    private String obec;
+
+    @Length(min = 5, max = 5)
+    @Pattern(regexp = "\\d{5}")
+    @NotBlank
+    private String psc;
+
+    @Length(max = 100)
+    @Email
+    private String email;
+
+    @Length(max = 20)
+    private String telefon;
+
+    @Length(max = 100)
+    private String web;
+
+    public Vizitka() {
+    }
+
+    public Vizitka(Integer id, String celeJmeno, String firma, String ulice, String obec, String psc, String email, String telefon, String web) {
+        this.id = id;
+        this.celeJmeno = celeJmeno;
+        this.firma = firma;
+        this.ulice = ulice;
+        this.obec = obec;
+        this.psc = psc;
+        this.email = email;
+        this.telefon = telefon;
+        this.web = web;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCeleJmeno() {
+        return celeJmeno;
+    }
+
+    public void setCeleJmeno(String celeJmeno) {
+        this.celeJmeno = celeJmeno;
+    }
+
+    public String getFirma() {
+        return firma;
+    }
+
+    public void setFirma(String firma) {
+        this.firma = firma;
+    }
+
+    public String getUlice() {
+        return ulice;
+    }
+
+    public void setUlice(String ulice) {
+        this.ulice = ulice;
+    }
+
+    public String getPsc() {
+        return psc;
+    }
+
+    public void setPsc(String psc) {
+        this.psc = psc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getObec() {
+        return obec;
+    }
+
+    public void setObec(String obec) {
+        this.obec = obec;
+    }
+
+    public String celaAdresa() {
+        return String.format("%s, %s, %s", ulice, psc, obec );
+    }
+}
